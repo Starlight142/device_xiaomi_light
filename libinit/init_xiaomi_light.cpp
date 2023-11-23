c1c3aaee71c3a0c2b586f0039bb06f5d0a0aa5c7
Taki / <93442834+AMRITO1@users.noreply.github.com>
2023-11-23 10:11:21 +0530

    light:Update init_xiaomi_light.cpp

--- libinit/init_xiaomi_light.cpp
+++ libinit/init_xiaomi_light.cpp
@@ -9,111 +9,89 @@
 
 #include "vendor_init.h"
 
-static const variant_info_t light_info = {
-    .bid_value = "S98016AA1",
-    .sku_value = "",
+static const variant_info_t lightcm_info = {
+    .hwc_value = "light",
+    .sku_value = "light",
+
+    .brand = "Redmi",
+    .device = "lightcm",
+    .model = "Redmi Note 11R",
+    .build_fingerprint = Redmi/light/light:13/ΤΡ1Α.220624.014/V14.0.5.0.TLSMIXM:user/release-keys",
+
+
+    .nfc = false,
+};
+
+static const variant_info_t lightin_info = {
+    .hwc_value = "light",
+    .sku_value = "light_global",
 
     .brand = "Redmi",
     .device = "light",
-    .name = "light",
-    .marketname = "Redmi Note 11R",
-    .model = "22095RA98C",
-    .build_fingerprint = "Redmi/light/light:13/ΤΡ1Α.220624.014/V14.0.5.0.TLSMIXM:user/release-keys",
+    .model = "Redmi Note 11E",
+    .build_fingerprint = Redmi/light/light:13/ΤΡ1Α.220624.014/V14.0.5.0.TLSMIXM:user/release-keys",
+
 
     .nfc = false,
 };
 
 static const variant_info_t light_info = {
-    .bid_value = "S98016BA1",
-    .sku_value = "",
+    .hwc_value = "lightcn",
+    .sku_value = "light",
 
     .brand = "Redmi",
     .device = "light",
-    .name = "light",
-    .marketname = "Redmi 11 Prime 5G",
-    .model =  "22041219I",
-    .build_fingerprint = "Redmi/light/light:13/ΤΡ1Α.220624.014/V14.0.5.0.TLSMIXM:user/release-keys",
+    .model = "Redmi 10 5G",
+    .build_fingerprint = "Redmi/light/light:13/ΤΡ1Α.220624.014/V14.0.5.0.TLSMIXM:user/release-keys"
 
     .nfc = false,
 };
 
-static const variant_info_t light_global_info = {
-    .bid_value = "S98016LA1",
-    .sku_value = "",
+static const variant_info_t lightgl_info = {
+    .hwc_value = "lightgl",
+    .sku_value = "light",
 
-    .brand = "POCO",
+    .brand = "Redmi",
     .device = "light",
-    .name = "light_global",
-    .marketname = "POCO M4 5G",
-    .model = " 22041219PG",
-    .build_fingerprint = "POCO/light/light_global:13/ΤΡ1Α.220624.014/V14.0.5.0.TLSMIXM:user/release-keys",
+    .model = "Redmi 10 prime+ 5G",
+    .build_fingerprint = "Redmi/light/light:13/ΤΡ1Α.220624.014/V14.0.5.0.TLSMIXM:user/release-keys"
 
     .nfc = false,
 };
 
-static const variant_info_t light_in_info = {
-    .bid_value = "S98017AA1",
-    .sku_value = "",
+static const variant_info_t lightin_info = {
+    .hwc_value = "lightin",
+    .sku_value = "light",
 
     .brand = "POCO",
     .device = "light",
-    .name = "light_in",
-    .marketname = "POCO M4 5G",
-    .model = "22041219PI",
+    .model = "POCO M4 5G",
     .build_fingerprint = "POCO/light/light_in:13/ΤΡ1Α.220624.014/V14.0.5.0.TLSMIXM:user/release-keys",
 
+
     .nfc = false,
 };
 
-static const variant_info_t light_global_info = {
-    .bid_value = "S98018AA1",
-    .sku_value = "",
+static const variant_info_t lightea_info = {
+    .hwc_value = "light",
+    .sku_value = "light",
 
     .brand = "Redmi",
-    .device = "light",
-    .name = "light_global",
-    .marketname = "Redmi 10 5G",
-    .model = "22041219G",
-    .build_fingerprint = "Redmi/light/light:13/ΤΡ1Α.220624.014/V14.0.5.0.TLSMIXM:user/release-keys",
-    .nfc = false,
-};
-static const variant_info_t light_global_info = {
-    .bid_value = "S98018AA1",
-    .sku_value = "",
+    .device = "lightcm",
+    .model = "Redmi 11 prime 5G",
+    .build_fingerprint = Redmi/light/light:13/ΤΡ1Α.220624.014/V14.0.5.0.TLSMIXM:user/release-keys",
 
-    .brand = "Redmi",
-    .device = "light",
-    .name = "light_global",
-    .marketname = "Redmi 10 Prime+ 5G",
-    .model = "22041219I",
-    .build_fingerprint = "Redmi/light/light:13/ΤΡ1Α.220624.014/V14.0.5.0.TLSMIXM:user/release-keys",
 
     .nfc = false,
 };
-static const variant_info_t light_global_info = {
-    .bid_value = "S98018AA1",
-    .sku_value = "",
-
-    .brand = "Redmi",
-    .device = "light",
-    .name = "light_global",
-    .marketname = "Redmi Note 11E",
-    .model = " 22041219C",
-    .build_fingerprint = "Redmi/light/light:13/ΤΡ1Α.220624.014/V14.0.5.0.TLSMIXM:user/release-keys",
-
-    .nfc = false,
-};
-
 
 static const std::vector<variant_info_t> variants = {
+    lightcm_info,
+    lightin_info,
     light_info,
-    light_info,
-    light_global_info,
-    light_in_info,
-    light_global_info,
-    light_global_info,    
-    light_global_info
-
+    lightgl_info,
+    lightin_info,
+    lightea_info,
 };
 
 void vendor_load_properties() {

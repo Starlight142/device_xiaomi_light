b4aa03d5a97d82008dc2ee2a2270ecf7ece5149f
Taki / <93442834+AMRITO1@users.noreply.github.com>
2023-11-23 12:23:27 +0530

    light:Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+git clone --depth=1 https://github.com/light-trees/vendor_xiaomi_light.git -b lineage-20 vendor/xiaomi/light
+
+git clone --depth=1 https://github.com/light-trees/device_xiaomi_light-kernel.git -b Lineage-20 device/xiaomi/light-kernel
+
+git clone --depth=1 https://github.com/light-trees/karnel_xiaomi_light.git -b 13 kernel/xiaomi/light
+
+git clone --depth=1 https://github.com/nashc-dev/android_hardware_mediatek.git hardware/mediatek
+
+git clone --depth=1 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
+
+git clone --depth=1 https://github.com/ArrowOS-Devices/android_hardware_xiaomi.git hardware/xiaomi

bb9763c9367154bcc9e88763a3a3f5f0ff9401f5
Taki / <93442834+AMRITO1@users.noreply.github.com>
2023-11-26 20:42:04 +0530

    light:Update lineage_light.mk

--- lineage_light.mk
+++ lineage_light.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/light/device.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aos/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_AOSP_RECOVERY := true
